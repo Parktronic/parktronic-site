bc1252705a7e2f817023dbfd56d305c90307d79e
Lisa Rozhkova <liza.roz@yandex.ru>
2024-04-26 16:11:57 +0300

    Fix bug with logout button

--- public/components/ProfileMenu/profile_menu.js
+++ public/components/ProfileMenu/profile_menu.js
@@ -1,5 +1,4 @@
 import { ROUTES } from '../../config.js';
-import { navbar } from '../Navbar/navbar.js';
 import { goToPage } from '../../modules/router.js';
 import { STORAGE } from '../../modules/storage.js';
 
@@ -15,22 +14,22 @@ export const renderProfileMenu = () => {
         return;
     }
 
-    const root = document.querySelector('#root');
-    root.innerHTML += Handlebars.templates.profile_menu();
+    const popup = document.querySelector('#popup');
+    popup.innerHTML = Handlebars.templates.profile_menu();
 
     // Функция убирающая рендер меню, а так же убирающая event listener клика по области вне нее
     const removeProfileMenu = (e) => {
         if (!e.target.classList.contains('navbar-profile-menu') &&
             !e.target.parentNode.classList.contains('navbar-profile-menu')) {
             document.body.removeEventListener('click', removeProfileMenu);
+            popup.innerHTML = '';
         }
     };
     document.body.addEventListener('click', removeProfileMenu);
 
-
     const logoutButton = document.querySelector('#navbar-menu-logout-button');
     logoutButton.addEventListener('click', () => {
-        document.body.removeEventListener('click', removeProfileMenu);
+        popup.innerHTML = '';
         goToPage(ROUTES.logout);
     });
 };
